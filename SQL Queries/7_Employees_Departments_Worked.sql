b5c806edfb134e49ab166bf659607466a1bfd2df
Joe Abraham <117947628+RockfordWayne@users.noreply.github.com>
2026-09-11 16:35:52 -0500

    Fix SQL join syntax for employee departments query

--- SQL Queries/7_Employees_Departments_Worked.sql
+++ SQL Queries/7_Employees_Departments_Worked.sql
@@ -3,8 +3,8 @@ SELECT
 	CONCAT(e.first_name, ' ' , e.last_name) AS full_name, 
 	COUNT(DISTINCT d.id) AS departments_worked
 FROM employees.employee AS e
-JOIN employees.department_employee AS de ON de.employee_id = e.id
-JOIN employees.department AS d ON de.department_id = d.id
+	JOIN employees.department_employee AS de ON de.employee_id = e.id
+	JOIN employees.department AS d ON de.department_id = d.id
 GROUP BY e.id, e.first_name, e.last_name
 HAVING COUNT(DISTINCT d.id) > 1
 ORDER BY departments_worked DESC, e.id ASC;
