7d7201b7595b136ee19951757407870324ba9e5d
Joe Abraham <117947628+RockfordWayne@users.noreply.github.com>
2026-09-11 16:34:00 -0500

    Refactor SQL query for readability and structure

--- SQL Queries/5_Employees_Above_Department_Average.sql
+++ SQL Queries/5_Employees_Above_Department_Average.sql
@@ -7,9 +7,12 @@
 
 
 WITH average_pay_CTE AS(
-	SELECT ROUND(AVG(s.amount), 2) AS average_pay, d.dept_name AS dept_name FROM employees.salary AS s
-	JOIN employees.department_employee AS de ON s.employee_id = de.employee_id
-	JOIN employees.department AS d ON de.department_id = d.id
+	SELECT 
+		ROUND(AVG(s.amount), 2) AS average_pay, 
+		d.dept_name AS dept_name 
+	FROM employees.salary AS s
+		JOIN employees.department_employee AS de ON s.employee_id = de.employee_id
+		JOIN employees.department AS d ON de.department_id = d.id
 	WHERE s.from_date = (
 		SELECT MAX(from_date)
 		FROM employees.salary AS s2
@@ -23,12 +26,17 @@ WITH average_pay_CTE AS(
 	GROUP BY d.dept_name
 )
 
-SELECT e.id, CONCAT(e.first_name, ' ' ,e.last_name) AS full_name, s.amount AS salary, d.dept_name, ap.average_pay
+SELECT 
+	e.id, 
+	CONCAT(e.first_name, ' ' ,e.last_name) AS full_name, 
+	s.amount AS salary, 
+	d.dept_name, 
+	ap.average_pay
 FROM employees.salary AS s
-JOIN employees.employee AS e ON s.employee_id = e.id
-JOIN employees.department_employee AS de ON e.id = de.employee_id
-JOIN employees.department AS d ON de.department_id = d.id
-JOIN average_pay_CTE AS ap ON d.dept_name = ap.dept_name
+	JOIN employees.employee AS e ON s.employee_id = e.id
+	JOIN employees.department_employee AS de ON e.id = de.employee_id
+	JOIN employees.department AS d ON de.department_id = d.id
+	JOIN average_pay_CTE AS ap ON d.dept_name = ap.dept_name
 WHERE s.from_date = (
 	SELECT MAX(from_date) 
 	FROM employees.salary AS s2
