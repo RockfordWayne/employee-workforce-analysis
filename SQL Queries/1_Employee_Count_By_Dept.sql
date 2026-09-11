ec0ea0c1eb79377775623326c7790996ce60038f
Joe Abraham <117947628+RockfordWayne@users.noreply.github.com>
2026-09-11 16:35:18 -0500

    Format SQL query for better readability

--- SQL Queries/1_Employee_Count_By_Dept.sql
+++ SQL Queries/1_Employee_Count_By_Dept.sql
@@ -1,6 +1,7 @@
-SELECT d.dept_name, COUNT(de.employee_id) AS employees 
+SELECT 
+    d.dept_name, 
+    COUNT(de.employee_id) AS employees 
 FROM employees.department_employee AS de 
-INNER JOIN employees.department AS d 
-ON de.department_id = d.id 
+  INNER JOIN employees.department AS d ON de.department_id = d.id 
 GROUP BY d.dept_name
 ORDER BY employees ASC;
